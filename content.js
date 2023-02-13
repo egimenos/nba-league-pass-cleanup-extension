1831a6b8daf86290c22a291bf184c4f3dd70c506
EgimenoS <ergisan@gmail.com>
2023-02-13 01:32:20 +0100

    hide overlay elements

--- content.js
+++ content.js
@@ -1,16 +1,59 @@
 window.onload = (function () {
-  const observer = new MutationObserver(function (
+  const spoilerImageObserver = new MutationObserver(function (
     _mutations,
     _mutationInstance
   ) {
+    console.log("wrapper");
     const backgroundWrapper = document.querySelector(".content-poster");
     if (backgroundWrapper) {
       backgroundWrapper.style.display = "none";
+      spoilerImageObserver.disconnect();
     }
   });
 
-  observer.observe(document, {
+  const videoControllerObserver = new MutationObserver(function (
+    _mutations,
+    _mutationInstance
+  ) {
+    const listenerContainer = document.querySelector(".full-progress-bar");
+    const videoControls = document.querySelector(".controlbarContainer");
+    const streamSelector = document.querySelector(".stream-selector");
+    const fullProgressBar = document.querySelector(".full-progress-bar");
+
+    /// if all elements are in the DOM, add event listener and disconnect oberver
+    if (
+      listenerContainer &&
+      videoControls &&
+      streamSelector &&
+      fullProgressBar
+    ) {
+      videoControllerObserver.disconnect();
+      listenerContainer.addEventListener("click", function (e) {
+        e.stopImmediatePropagation();
+
+        const dataHide = videoControls.getAttribute("data-hide");
+        videoControls.setAttribute("data-hide", toggleAttribute(dataHide));
+        streamSelector.setAttribute("data-hide", toggleAttribute(dataHide));
+        toggleAttribute(dataHide) === "true"
+          ? (fullProgressBar.style.opacity = 0)
+          : (fullProgressBar.style.opacity = 0.9);
+      });
+    }
+  });
+
+  videoControllerObserver.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+
+  spoilerImageObserver.observe(document, {
     childList: true,
     subtree: true,
   });
 })();
+
+const toggleAttribute = (stringyBoolean) => {
+  if (stringyBoolean === "true") {
+    return "false";
+  } else return "true";
+};
